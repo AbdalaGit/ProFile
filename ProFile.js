a71a27eb20fe104df0f0b680cac8cac83b6cfba7
abdala <abdalapepo1234@gmail.com>
2022-09-05 20:26:17 +0200

    editeing

--- ProFile.js
+++ ProFile.js
@@ -1,5 +1,31 @@
 let ProgressSpan = document.querySelectorAll(".progress span");
 let who = document.querySelector(".who-am");
+let Body = document.getElementById("dark");
+let ImgDark = document.getElementById("img-dark");
+let buttom = document.querySelector(".darkk");
+let mod = "dark";
+
+buttom.addEventListener('click', () => {
+  Body.classList.toggle("dark");
+  buttom.innerHTML = "night";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.onscroll = function(){
   if(window.scrollY >= who.offsetTop - 50){
@@ -15,7 +41,7 @@ function loading(){
 };
 
 function addloading(){
-  setInterval(loading, 2500);
+  setInterval(loading, 1000 );
 };
 
 window.onload = addloading();
