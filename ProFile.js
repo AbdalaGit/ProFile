24136716e7440b2a8b161ed5513f99c4b865ee1c
abdala <abdalapepo1234@gmail.com>
2022-09-27 00:19:56 +0200

    dddfff

--- ProFile.js
+++ ProFile.js
@@ -3,45 +3,38 @@ let who = document.querySelector(".who-am");
 let Body = document.getElementById("dark");
 let ImgDark = document.getElementById("img-dark");
 let buttom = document.querySelector(".darkk");
-let mod = "dark";
 
-buttom.addEventListener('click', () => {
-  Body.classList.toggle("dark");
-  buttom.innerHTML = "night";
+buttom.addEventListener("click", () => {
+  Body.className = "dark";
+  buttom.textContent = "night";
+  buttom.src = "night.png";
+
+  if(buttom.textContent == "night" && Body.className == "dark"){
+    buttom.onclick = () =>{
+      Body.className = "night";
+      buttom.textContent = "dark";
+      if(buttom.textContent == "dark" && Body.className == "night"){
+      }
+    }
+  }
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.onscroll = function(){
-  if(window.scrollY >= who.offsetTop - 50){
-      ProgressSpan.forEach((span => {
+window.onscroll = function () {
+  if (window.scrollY >= who.offsetTop - 50) {
+    ProgressSpan.forEach((span) => {
       span.style.width = span.dataset.width;
-    }));
-  };
+    });
+  }
 };
 
-
-function loading(){
+function loading() {
   document.querySelector(".loading").classList.add("stop");
-};
+}
 
-function addloading(){
-  setInterval(loading, 2500 );
-};
+function addloading() {
+  setInterval(loading, 2500);
+}
 
 window.onload = addloading();
